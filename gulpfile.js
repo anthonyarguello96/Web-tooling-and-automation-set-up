9af0a9b5d08906782a6cdfec3a311962c6c9f6bb
anthonyarguello96 <anthonyarguello96@gmail.com>
2020-03-27 09:23:54 -0400

    Set up autoprefixer
    Install autroprefixer as a dependecy
    Create a variable that require gulp-autoprefixer
    Set up browserlist to the las tow version
    Set up watch task for sass files (not sure is this feature is working).

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
 const gulp = require("gulp");
 const sass = require ("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+
 
 gulp.task("default", function(){
-  //Code for default task
+  gulp.watch("sass/**/*.scss", gulp.series('styles'));
 });
 
 gulp.task("styles", function(){
   return gulp.src("sass/**/*.scss")
     .pipe(sass())
     .on("error", sass.logError)
+    .pipe(
+      autoprefixer({
+        browserlist: ["last 2 versions"]
+      }))
     .pipe(gulp.dest("./css"));
 });
