137f8309c8c7b8865410ebfaf53fabeda6f800ad
anthonyarguello96 <anthonyarguello96@gmail.com>
2020-03-27 08:16:52 -0400

    Set up sass
    Install plug in
    Create a sass directory and move the css files to this directory
    Switch the .css for .scss
    Create a variable that require gulp-sass in the gulpfile
    Create a styles task  in the gulpfile as well

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require("gulp");
-
+const sass = require ("gulp-sass");
 
 gulp.task("default", function(){
   //Code for default task
+});
+
+gulp.task("styles", function(){
+  return gulp.src("sass/**/*.scss")
+    .pipe(sass())
+    .on("error", sass.logError)
+    .pipe(gulp.dest("./css"));
 });
