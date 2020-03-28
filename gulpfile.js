3b9fbf64aa275a1532700f19e9ba95bda12540aa
anthonyarguello96 <anthonyarguello96@gmail.com>
2020-03-28 18:48:34 -0400

    Set up live editing
    Install browserSync
    Create variable to require browserSync
    Modify the default task to initialize the server
    Add a variable to define reload
    Add watch task to lookout for html and js changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 const gulp = require("gulp");
 const sass = require ("gulp-sass");
 const autoprefixer = require("gulp-autoprefixer");
-
+const browserSync = require("browser-sync").create();
+const reload = browserSync.reload;
 
 gulp.task("default", function(){
-  gulp.watch("sass/**/*.scss", gulp.series('styles'));
+  gulp.watch("sass/**/*.scss", gulp.series('styles', reload));
+  browserSync.init({
+    server: "./"
+  });
+  gulp.watch("*.html").on("change", reload);
+  gulp.watch("js/**/*.js").on("change", reload);
 });
+
 
 gulp.task("styles", function(){
   return gulp.src("sass/**/*.scss")
@@ -15,5 +22,6 @@ gulp.task("styles", function(){
       autoprefixer({
         browserlist: ["last 2 versions"]
       }))
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
 });
