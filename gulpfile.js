aee728eec5429a6e625c022ffc829c1cd67f6f81
anthonyarguello96 <anthonyarguello96@gmail.com>
2020-03-29 13:51:46 -0400

    Set up Eslint
    Install linter and linter-Eslint packages in atom.
    Create a .eslintrc file in the root directory
    Save Eslint as a dependency
    Define rules in the .eslintrc file

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
-const gulp = require("gulp");
-const sass = require ("gulp-sass");
-const autoprefixer = require("gulp-autoprefixer");
-const browserSync = require("browser-sync").create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
-gulp.task("default", function(){
-  gulp.watch("sass/**/*.scss", gulp.series('styles', reload));
+gulp.task('default', function() {
+  gulp.watch('sass/**/*.scss', gulp.series('styles', reload));
   browserSync.init({
-    server: "./"
+    server: './',
   });
-  gulp.watch("*.html").on("change", reload);
-  gulp.watch("js/**/*.js").on("change", reload);
+  gulp.watch('*.html').on('change', reload);
+  gulp.watch('js/**/*.js').on('change', reload);
 });
 
 
-gulp.task("styles", function(){
-  return gulp.src("sass/**/*.scss")
-    .pipe(sass())
-    .on("error", sass.logError)
-    .pipe(
-      autoprefixer({
-        browserlist: ["last 2 versions"]
-      }))
-    .pipe(gulp.dest("./css"))
-    .pipe(browserSync.stream());
+gulp.task('styles', function() {
+  return gulp.src('sass/**/*.scss')
+      .pipe(sass())
+      .on('error', sass.logError)
+      .pipe(
+          autoprefixer({
+            browserlist: ['last 2 versions'],
+          }))
+      .pipe(gulp.dest('./css'))
+      .pipe(browserSync.stream());
 });
