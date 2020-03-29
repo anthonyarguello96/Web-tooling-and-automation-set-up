ac83a4779ed9bff0b391d238fd1e17363ef0e471
anthonyarguello96 <anthonyarguello96@gmail.com>
2020-03-29 15:18:32 -0400

    Set up ESLint in gulp
    Install gulp-eslint into workspace
    Add a variable in the gulpfile.js tha require gulp-eslint
    Create a lint task
    Create a gulp.watch task to look out for changes in the js
    and run the lint task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
+const eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
+gulp.task('default', () => {
   gulp.watch('sass/**/*.scss', gulp.series('styles', reload));
+  gulp.watch('js/**/*.js', gulp.series('lint', reload));
   browserSync.init({
     server: './',
   });
@@ -14,7 +16,7 @@ gulp.task('default', function() {
 });
 
 
-gulp.task('styles', function() {
+gulp.task('styles', () => {
   return gulp.src('sass/**/*.scss')
       .pipe(sass())
       .on('error', sass.logError)
@@ -24,4 +26,17 @@ gulp.task('styles', function() {
           }))
       .pipe(gulp.dest('./css'))
       .pipe(browserSync.stream());
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['js/**/*.js'])
+  // eslint() attaches the lint output to the "eslint" property
+  // of the file object so it can be used by other modules.
+      .pipe(eslint())
+  // eslint.format() outputs the lint results to the console.
+  // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+  // To have the process exit with an error code (1) on
+  // lint error, return the stream and pipe to failAfterError last.
+      .pipe(eslint.failAfterError());
 });
