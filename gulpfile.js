82e1466e3238350a9ecce33f881a4b73059a03f5
anthonyarguello96 <anthonyarguello96@gmail.com>
2020-04-05 15:06:19 -0400

    Migrate to gulp 4
    This commit also separates the generated files from the source files

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,45 @@
 const gulp = require('gulp');
+// const {series} = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 const eslint = require('gulp-eslint');
 const jasmineBrowser = require('gulp-jasmine-browser');
-const watch = require('gulp-watch');
+// const watch = require('gulp-watch');
 
 
-gulp.task('default', () => {
-  gulp.watch('sass/**/*.scss', gulp.series('styles', reload));
-  gulp.watch('js/**/*.js', gulp.series('lint', reload));
+function watch() {
   browserSync.init({
-    server: './',
+    server: './dist',
   });
-  gulp.watch('*.html').on('change', reload);
+  gulp.watch('sass/**/*.scss', gulp.parallel(styles));
+  gulp.watch('sass/**/*.scss').on('change', reload);
+  gulp.watch('js/**/*.js', gulp.series(lint));
   gulp.watch('js/**/*.js').on('change', reload);
-});
+  gulp.watch('index.html', gulp.parallel(copyHtml));
+  gulp.watch('*.html').on('change', reload);
+  gulp.watch('img/*', gulp.parallel(copyImages));
+  gulp.watch('img/*').on('change', reload);
+}
 
 
-gulp.task('styles', () => {
-  return gulp.src('sass/**/*.scss')
+function styles(cb) {
+  gulp.src('sass/**/*.scss')
       .pipe(sass())
       .on('error', sass.logError)
       .pipe(
           autoprefixer({
             browserlist: ['last 2 versions'],
           }))
-      .pipe(gulp.dest('./css'))
+      .pipe(gulp.dest('dist/css'))
       .pipe(browserSync.stream());
-});
+  cb();
+}
 
-gulp.task('lint', () => {
-  return gulp.src(['js/**/*.js'])
+
+function lint(cb) {
+  gulp.src(['js/**/*.js'])
   // eslint() attaches the lint output to the "eslint" property
   // of the file object so it can be used by other modules.
       .pipe(eslint())
@@ -42,13 +49,36 @@ gulp.task('lint', () => {
   // To have the process exit with an error code (1) on
   // lint error, return the stream and pipe to failAfterError last.
       .pipe(eslint.failAfterError());
-});
+  cb();
+}
 
 
-gulp.task('jasmine', () => {
-  const filesForTest = ['js/**/*.js', 'spec/**/*_spec.js'];
-  return gulp.src(filesForTest)
-      .pipe(watch(filesForTest))
+gulp.task('jasmine', function() {
+  return gulp.src(['js/**/*.js', 'spec/**/*_spec.js'])
       .pipe(jasmineBrowser.specRunner())
       .pipe(jasmineBrowser.server({port: 8888}));
 });
+
+
+function copyHtml(cb) {
+  gulp.src('index.html')
+      .pipe(gulp.dest('dist'));
+  cb();
+}
+
+function copyImages(cb) {
+  gulp.src('img/')
+      .pipe(gulp.dest('dist/img'));
+  cb();
+}
+
+gulp.task('default', gulp.parallel(styles, watch, lint,
+    copyHtml, copyImages, 'jasmine'));
+
+exports.copyHtml = copyHtml;
+exports.copyImages = copyImages;
+exports.styles = styles;
+exports.lint = lint;
+// exports.jasmine = jasmine;
+
+// note: Keep an eyeon the function copyImages and its path.
